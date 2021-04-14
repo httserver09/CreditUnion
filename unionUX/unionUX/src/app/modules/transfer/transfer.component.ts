936dade771053de8dacbb2a436a8076730089fbb
Joshua Soladoye <Joshua.Soladoye@absa.africa>
2021-04-14 10:23:05 +0200

    + beneficiary syncing with payment (with actual payment incomplete)

--- unionUX/unionUX/src/app/modules/transfer/transfer.component.ts
+++ unionUX/unionUX/src/app/modules/transfer/transfer.component.ts
@@ -8,6 +8,12 @@ interface Food {
   viewValue: string;
 }
 
+interface Account {
+  value: number;
+  valuecount: string;
+  viewValue: string;
+}
+
 
 @Component({
   selector: 'app-transfer',
@@ -30,6 +36,11 @@ export class TransferComponent implements OnInit {
     {value: 'Bank Of America', viewValue: 'Bank Of America'}
   ];
 
+  accounts: Account[] = [
+    {  value: 1, valuecount: '1 (Savings Account) ', viewValue: 'Savings account'},
+    {  value: 2, valuecount: '2 (Cheque Account) ', viewValue: 'Checking account'}
+  ];
+
   constructor(private _formBuilder: FormBuilder,
               private beneficiaryService: BeneficiaryService) { }
               
@@ -38,6 +49,7 @@ export class TransferComponent implements OnInit {
               accountNumber = new FormControl({ value: null, disabled: true });
               yourRef = new FormControl({ value: null, disabled: true });
               beneficiaryReference = new FormControl({ value: null, disabled: true });
+              accountId = new FormControl({value: '', disabled: true}) 
 
   ngOnInit(): void {
     this.firstFormGroup = this._formBuilder.group(
@@ -47,7 +59,7 @@ export class TransferComponent implements OnInit {
         accountNumber: ['', Validators.required],
         yourRef: ['', Validators.required],
         beneficiaryReference: ['', Validators.required],
-        accId: new FormControl({value: '1', disabled: true})
+        accountId: ['', Validators.required ]
       }
     );
 
@@ -58,25 +70,25 @@ export class TransferComponent implements OnInit {
         accountNumber: new FormControl({value: '', disabled: true}, Validators.required),
         yourRef: new FormControl({value: '', disabled: true}, Validators.required),
         beneficiaryReference: new FormControl({value: '', disabled: true}, Validators.required),  
-        accId: new FormControl({value: '1', disabled: true}) 
+        accountId: new FormControl({value: '', disabled: true}, Validators.required) 
       }
     );
   }
 
   onSubmit()
   {
-    alert(this.firstFormGroup.value);
-    console.log('First Form: ' + this.firstFormGroup.value);
-    console.log('First Form: ' + this.firstFormGroup.value);
+    alert(this.firstFormGroup);
+
+    console.log('First Form: ' + this.firstFormGroup);
 
     this.secondFormGroup = this.firstFormGroup;
 
     console.log('Second Form: ' + this.secondFormGroup);
   }
 
-  onSubmitSecond(){
-    console.log('First form: ' + this.firstFormGroup.value);
-    console.log('Second form: ' + this.secondFormGroup.value);
+  onSubmitSecond() {
+    console.log('First form: ' + this.firstFormGroup);
+    console.log('Second form: ' + this.secondFormGroup);
     // this.beneficiaryService.addBeneficiary(this.secondFormGroup.value);
 
     this.beneficiaryService.addBeneficiary(this.secondFormGroup.value).subscribe(data => {
